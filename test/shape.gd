a34de21b369ac14e435b83be3ab315f1f3526b85
Luis Cabañero <Luis.Cabanero@uclm.es>
2024-08-16 20:33:42 +0200

    Base logic for the main mechanic

---
+++ test/shape.gd
@@ -0,0 +1,27 @@
+extends RigidBody2D
+
+const Shape = preload("res://test/shape.tscn")
+@onready var timer := $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.start()
+	print(rad_to_deg(($RayCast2D.target_position - $RayCast2D.position).angle()))
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_timer_timeout():
+	var new_shape := duplicate()
+	new_shape.global_position = $RayCast2D.global_position
+	new_shape.global_rotation = ($RayCast2D.target_position - $RayCast2D.position).angle() + global_rotation
+	print(new_shape.global_position, " ", new_shape.global_rotation_degrees)
+	print(new_shape.get_children())
+	new_shape.get_child(1).start()
+
+	get_parent().add_child(new_shape)
