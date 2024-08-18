a182c417b6cf5d4659a285cbacfbd33dd6d4640e
Luis Cabañero <Luis.Cabanero@uclm.es>
2024-08-18 13:04:50 +0200

    Renamed aux folder to elements to avoid problems.

--- game_elements/shapes/shape.gd
+++ game_elements/shapes/shape.gd
@@ -3,12 +3,18 @@ extends RigidBody2D
 
 @export var icon : Texture2D
 @export var replica_arrows : Array[ReplicaArrow]
+@export var border_width : float = 4
 
 @onready var shape := $shape
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	shape.polygon = $collision.polygon
+	var inner_pol : PackedVector2Array = shape.polygon.duplicate()
+	for i in len(inner_pol):
+		
+		inner_pol[i] *= .9
+	$shape/inner_shape.polygon = inner_pol
 
 
 func _on_replica_arrows_child_entered_tree(node: Node):
