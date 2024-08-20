28702b08c574b3c1b8aa0bf6ba9cb6e8614ab8c2
Luis Cabañero <Luis.Cabanero@uclm.es>
2024-08-20 13:18:14 +0200

    Deleted warnings

--- signal_bus.gd
+++ signal_bus.gd
@@ -1,12 +1,15 @@
 extends Node
 
+@warning_ignore("unused_signal")
 signal game_lost(reason)
 #emited lose_game_zone , 
 
 
+@warning_ignore("unused_signal")
 signal game_won
 #emited: target_zone
 
 
+@warning_ignore("unused_signal")
 signal activate_delete_areas(value)
 #emited: level_interface
